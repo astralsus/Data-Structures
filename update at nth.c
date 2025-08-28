08202b671438039f9697cf3e44112a3eb162c15d
Amullya Ratna Verma <123181386+astralsus@users.noreply.github.com>
2025-08-28 23:29:18 +0530

    update at nth.c

---
+++ update at nth.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int pos = 2;
+    int newl = 99;
+
+    if(pos >= 0 && pos < 5) {
+        arr[pos] = newl;
+    }
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
